da7d4c109df036e031febd1bf963172b9b4525dc
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-04-05 21:20:44 +0300

    Lint

--- library/Hasql/OptparseApplicative.hs
+++ library/Hasql/OptparseApplicative.hs
@@ -1,6 +1,6 @@
 module Hasql.OptparseApplicative where
 
-import BasePrelude hiding (option)
+import BasePrelude
 import qualified Hasql.Connection as A
 import qualified Hasql.Pool as B
 import Options.Applicative
